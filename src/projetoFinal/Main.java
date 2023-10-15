608871279c3056e8d48565f1b567f66d9486b836
Alex da Silva Alves <AlexSilvaAlves17@outlook.com>
2023-10-14 23:32:45 -0300

    Main view design

--- src/projetoFinal/Main.java
+++ src/projetoFinal/Main.java
@@ -2,8 +2,8 @@ package projetoFinal;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -15,9 +15,13 @@ public class Main extends Application {
 	@Override
 	public void start(Stage stage) throws Exception {
 		try {
-			Parent root = FXMLLoader
+			ScrollPane root = FXMLLoader
 					.load(getClass()
 					.getResource(caminho));
+			
+			root.setFitToHeight(true);
+			root.setFitToWidth(true);
+			
 			Scene scene = new Scene(root);
 			stage.setScene(scene);
 			stage.setTitle("Projeto JavaFx Simples");
